191e2c4a0fb8cb919c143e906ecf09d917adb227
Frida Pedersén <fp222ni@student.lnu.se>
2024-10-24 11:02:32 +0200

    optimize carApp.js, format

--- src/js/components/car-application/carApplication.js
+++ src/js/components/car-application/carApplication.js
@@ -63,91 +63,113 @@ template.innerHTML = `
       border-radius: 6px;
     }
 
-  </style>
+    #buttonContainer {
+      align-items: center;
+    }
 
+  </style>
+  <div id="buttonContainer">
   <button id="createBookingButton" tabindex="0">Create booking</button>
-  <div id="bookingForm"></div>
-  <button id="createCustomerButton" tabindex="0">Create new customer</button>
   <button id="viewBookingButton" tabindex="0">View your booking</button>
-  <div id="customerForm"></div>
+  </div>
+  <div id="bookingForm"></div>
   <div id="productList"></div>
 
   `
 customElements.define('car-application',
-  /**
-   * Represents a car-app element.
-   */
+
   class extends HTMLElement {
     #bookingForm
-    #customerForm
     #productList
     #createBookingButton
-    #createCustomerButton
     #viewBookingButton
     #bookings = []
     #products = []
     #bookingManager
     #bookingListing
 
-    /**
-     * Creates an instance of the current type.
-     */
+
     constructor() {
       super()
 
+      this.#initializeBookingManager()
+      this.#attachTemplate()
+      this.#initializeElements()
+      this.#fetchProducts()
+
+
+      this.#bookingForm.setProducts(this.#products)
+    }
+
+    connectedCallback() {
+      this.#setupEventListeners()
+    }
+
+    #setupEventListeners() {
+      this.#createBookingButton.addEventListener('click', this.#showBookingForm.bind(this))
+      this.#viewBookingButton.addEventListener('click', this.#showBookingListing.bind(this))
+    }
+
+    #showBookingForm() {
+      this.#toggleVisibility(this.#createBookingButton, false)
+      this.#toggleVisibility(this.#bookingForm, true)
+      this.#toggleVisibility(this.#productList, false)
+      this.#toggleVisibility(this.#viewBookingButton, false)
+    }
+
+    #initializeBookingManager() {
       const storage = new LocalStorageAdapter()
       this.#bookingManager = new BookingManager(storage)
+    }
 
-      // Attach a shadow DOM tree to this element and
-      // append the template to the shadow root.
-      this.attachShadow({ mode: 'open' })
-        .appendChild(template.content.cloneNode(true))
+    #attachTemplate() {
+      this.attachShadow({ mode: 'open' }).appendChild(template.content.cloneNode(true))
+    }
 
-      this.fetchProducts()
-
-      const productList = document.createElement('product-listing')
-      this.shadowRoot.appendChild(productList)
-
-      this.#productList = productList
-
+    #initializeElements() {
       this.#createBookingButton = this.shadowRoot.querySelector('#createBookingButton')
       this.#viewBookingButton = this.shadowRoot.querySelector('#viewBookingButton')
 
-      const bookingForm = document.createElement('booking-form')
+      this.#productList = this.#createElement('product-listing')
+      this.#bookingForm = this.#createBookingForm()
+      this.#bookingListing = this.#createElement('booking-listing')
+
+      this.#hideElements([this.#bookingForm, this.#bookingListing])
+    }
+
+    #createElement(tag) {
+      const element = document.createElement(tag)
+      this.shadowRoot.appendChild(element)
+      return element
+    }
+
+    #hideElements(elements) {
+      elements.forEach(element => element.style.display = 'none')
+    }
+
+    #showElements(elements) {
+      elements.forEach(element => element.style.display = 'block')
+    }
+
+    #showBookingListing() {
+      this.#toggleVisibility(this.#bookingListing, true)
+      this.#toggleVisibility(this.#productList, false)
+      this.#bookingListing.initialize(this.#bookings)
+    }
+
+    #createBookingForm() {
+      const bookingForm = this.#createElement('booking-form')
       bookingForm.setProducts(this.#products)
-      this.shadowRoot.appendChild(bookingForm)
-      this.#bookingForm = bookingForm
-      this.#bookingForm.style.display = 'none'
-      this.#bookingForm.addEventListener('bookingRequestAdded', this.handleBookingAdded.bind(this));
-
-      const bookingListing = document.createElement('booking-listing')
-      this.shadowRoot.appendChild(bookingListing)
-      this.#bookingListing = bookingListing
-      this.#bookingListing.style.display = 'none'
-
-
-
-
-      const customerForm = document.createElement('customer-form')
-      this.shadowRoot.appendChild(customerForm)
-
-      this.#customerForm = customerForm
+      bookingForm.addEventListener('bookingRequestAdded', this.#handleBookingAdded.bind(this))
+      return bookingForm
     }
 
-    /**
-     * Called when the element is inserted to the DOM.
-     */
-    connectedCallback() {
-      this.#createBookingButton.addEventListener('click', this.handleCreateBookingClick.bind(this))
-      this.#viewBookingButton.addEventListener('click', this.handleViewBookingClick.bind(this))
-    }
-
-    toggleVisibility(element, shouldShow) {
+    #toggleVisibility(element, shouldShow) {
       element.style.display = shouldShow ? 'block' : 'none'
     }
 
 
-    async fetchProducts() {
+    async #fetchProducts() {
       try {
         const response = await fetch('products.json')
         this.fetchedProducts = await response.json()
@@ -164,56 +186,34 @@ customElements.define('car-application',
       }
     }
 
-    handleCreateBookingClick() {
-      this.toggleVisibility(this.#createBookingButton, false)
-      this.toggleVisibility(this.#bookingForm, true)
-      this.toggleVisibility(this.#productList, false)
-      this.toggleVisibility(this.#viewBookingButton, false)
-
-    }
-
-    async handleBookingAdded(event) {
+    async #handleBookingAdded(event) {
       const { customer, selectedProduct, selectedDate } = event.detail;
 
-      const newCustomer = await this.saveCustomer(customer)
+      const newCustomer = await this.#saveCustomer(customer)
 
-      const customerId = newCustomer.id
-
-      const productId = selectedProduct.id
-
-      const bookingData = { customerId, productId, selectedDate }
-
-      const newBooking = await this.saveBooking(bookingData)
+      const newBooking = await this.#saveBooking({
+        customerId: newCustomer.id,
+        productId: selectedProduct.id,
+        selectedDate
+      })
 
       this.#bookings.push(newBooking)
 
       // Perform any additional actions you want here, e.g., showing a message
       alert(`Booking complete! You will recieve an email with the details of your booking. \nChosen car: ${newBooking.product.name}\nDate booked: ${newBooking.date}\nYour email: ${newBooking.customer.email}`)
-      this.toggleVisibility(this.#bookingForm, false)
-      this.toggleVisibility(this.#productList, true)
-      this.toggleVisibility(this.#viewBookingButton, true)
-      this.toggleVisibility(this.#createBookingButton, true)
+      this.#showElements([this.#viewBookingButton, this.#createBookingButton, this.#productList])
+      this.#hideElements([this.#bookingForm])
     }
 
-    async saveBooking(bookingData) {
+    async #saveBooking(bookingData) {
       console.log('all', bookingData.productId)
       const newBooking = await this.#bookingManager.addBooking(bookingData.productId, bookingData.customerId, bookingData.selectedDate)
       return newBooking
     }
 
-    async saveCustomer(customer) {
+    async #saveCustomer(customer) {
       const newCustomer = await this.#bookingManager.addCustomer(customer)
       return newCustomer
-    }
-
-    async handleViewBookingClick() {
-      this.toggleVisibility(this.#bookingListing, true)
-      this.toggleVisibility(this.#productList, false)
-      console.log('bookings being sent', this.#bookings)
-
-      const bookings = this.#bookings
-      this.#bookingListing.initialize(bookings)
-
     }
 
   })
